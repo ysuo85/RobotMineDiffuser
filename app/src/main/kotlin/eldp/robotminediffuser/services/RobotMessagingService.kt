c570495c77b46689b24070e113c829ebd9c9e05a
ysuo85 <yi.suo@stonybrook.edu>
2017-07-20 22:12:52 -0400

    - Implements RobotMessagingService that automatically pairs and connects to Arduino on start of the app. - Adds timestamp to ArduinoMessage.

--- app/src/main/kotlin/eldp/robotminediffuser/services/RobotMessagingService.kt
+++ app/src/main/kotlin/eldp/robotminediffuser/services/RobotMessagingService.kt
@@ -2,137 +2,241 @@ package eldp.robotminediffuser.services
 
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothClass
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
-import android.os.Handler
-import android.os.IBinder
+import android.content.IntentFilter
+import android.os.*
 import android.util.Log
+import android.widget.Toast
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import eldp.robotminediffuser.data.ArduinoMessage
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
-import java.util.*
 
 class RobotMessagingService : Service() {
-
-    companion object {
-        @JvmStatic val BT_DEVICE : String = "bt_device"
-        @JvmStatic val SPP_UUID : String = "00001101-0000-1000-8000-00805F9B34FB"
-        @JvmStatic var mState : RobotMessagingStates = RobotMessagingStates.StateNone
-        @JvmStatic var DeviceName : String = ""
-        @JvmStatic var device : BluetoothClass.Device? = null
+    private val mHandler: Handler? = null // handler that gets info from Bluetooth service
+    private val mMessenger: Messenger
+    private var mRobotConnection : ConnectedThread ?= null
+    private val mapper = jacksonObjectMapper()
+    private val mBluetoothAdapter : BluetoothAdapter
+    private val mReceiver : BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context : Context, intent : Intent){
+            val action = intent.action
+            if(BluetoothDevice.ACTION_FOUND == action){
+                val device : BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                if(device.name == ARDUINO_BT_NAME){
+                    ConnectThread(device).start()
+                }
+            }
+        }
     }
 
-    val mBluetoothAdapter : BluetoothAdapter? = null
-    private var mConnectThread : ConnectThread? = null
-    private var mConnectedThread : ConnectedThread? = null
-    private var mBinder: IBinder? = null
+    init {
+        mMessenger = Messenger(IncomingHandler())
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    }
 
     override fun onCreate() {
-        Log.d("RobotMessagingService", "Service started")
-        super.onCreate()
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mReceiver, filter)
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        return mBinder
-    }
-
-    enum class RobotMessagingStates {
-        StateNone,
-        StateListen,
-        StateConnecting,
-        StateConnected
-    }
-
-    @Synchronized private fun connected(mmDevice: BluetoothDevice, mmSocket: BluetoothSocket){
-        if(mConnectThread != null){
-            mConnectThread!!.cancel()
-            mConnectThread = null
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if(mBluetoothAdapter == null){
+            Toast.makeText(applicationContext, "Bluetooth unsupported", Toast.LENGTH_SHORT).show()
         }
+        else{
+            if(!mBluetoothAdapter.isEnabled){
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivity(enableBtIntent)
+            }
 
-        if(mConnectedThread != null){
- ///           mConnectedThread.cancel()
-            mConnectedThread = null
+            // Attempt to obtain a list of all presently paired devices
+            val pairedDevices = mBluetoothAdapter.bondedDevices
+            var deviceFound = false
+            if(pairedDevices.size > 0){
+                for(device : BluetoothDevice in pairedDevices){
+                    // Find the device with matching name and attempt to connect to it
+                    if(device.name == ARDUINO_BT_NAME){
+                        ConnectThread(device).start()
+                        deviceFound = true
+                        break
+                    }
+                }
+            }
+            if(!deviceFound){
+                mBluetoothAdapter.startDiscovery()
+            }
         }
-
-        mConnectedThread = ConnectedThread(mmSocket)
-        mConnectedThread!!.start()
-        mState = RobotMessagingStates.StateConnected
+        return START_STICKY
     }
 
-    private class ConnectThread(device: BluetoothDevice, adapter : BluetoothAdapter) : Thread() {
-        private var mmSocket : BluetoothSocket? = null
-        private var mmDevice : BluetoothDevice? = null
-        private var mmBluetoothAdapter : BluetoothAdapter? = null
+    override fun onBind(intent : Intent?): IBinder {
+        Toast.makeText(applicationContext, "binding", Toast.LENGTH_SHORT).show()
+        return mMessenger.binder
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRobotConnection?.cancel()
+        unregisterReceiver(mReceiver)
+    }
+
+    // Defines several constants used when transmitting messages between the
+    // service and the UI.
+    private interface MessageConstants {
+        companion object {
+            val MESSAGE_READ = 0
+            val MESSAGE_WRITE = 1
+            val MESSAGE_TOAST = 2
+        }
+    }
+
+    private inner class IncomingHandler : Handler() {
+        override fun handleMessage(msg: Message?) {
+            val llArduinoMessage : ArduinoMessage = msg!!.data.getSerializable("command") as ArduinoMessage
+            mRobotConnection?.write(mapper.writeValueAsBytes(llArduinoMessage))
+        }
+    }
+
+    private inner class ConnectThread(mmDevice: BluetoothDevice) : Thread() {
+        private val mmSocket: BluetoothSocket
 
         init {
-            this.mmBluetoothAdapter = adapter
-            this.mmDevice = device
-            var tmp : BluetoothSocket? = null
-            try{
-                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString(SPP_UUID))
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            var tmp: BluetoothSocket? = null
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = mmDevice.createRfcommSocketToServiceRecord(mmDevice.uuids[0].uuid)
+            } catch (e: IOException) {
+                Log.e(TAG, "Socket's create() method failed", e)
             }
-            catch (e : IOException) {
-                e.printStackTrace()
-            }
-            mmSocket = tmp
+            mmSocket = tmp!!
         }
 
-        override fun run(){
-            name = "ConnectThread"
-            mmBluetoothAdapter!!.cancelDiscovery()
-            try{
-                mmSocket?.connect()
-            }
-            catch(e : IOException){
-                try{
-                    mmSocket?.close()
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            mBluetoothAdapter.cancelDiscovery()
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect()
+            } catch (connectException: IOException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close()
+                } catch (closeException: IOException) {
+                    Log.e(TAG, "Could not close the client socket", closeException)
                 }
-                catch(e1 : IOException){
-                    e1.printStackTrace()
-                }
-  //              connectionFailed()
                 return
             }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            mRobotConnection = ConnectedThread(mmSocket)
+            mRobotConnection!!.start()
         }
 
-        fun cancel(){
-            try{
-                mmSocket?.close()
-            }
-            catch(e : IOException)
-            {
-                Log.e("RobotMessagingService", "close() of connect socket failed")
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the client socket", e)
             }
         }
     }
 
-    private class ConnectedThread(socket : BluetoothSocket) : Thread() {
-        private var mmSocket : BluetoothSocket? = null
-        private var mmInStream : InputStream? = null
-        private var mmOutStream : OutputStream? = null
+    private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+        private val mmInStream: InputStream
+        private val mmOutStream: OutputStream
+        private var mmBuffer: ByteArray? = null // mmBuffer store for the stream
 
         init {
-            mmSocket = socket
             var tmpIn: InputStream? = null
             var tmpOut: OutputStream? = null
+
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
             try {
-                tmpIn = socket.inputStream
-                tmpOut = socket.outputStream
-            } catch(e: IOException) {
-                Log.e("RobotMessagingService", "temp socket not created", e)
+                tmpIn = mmSocket.inputStream
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when creating input stream", e)
+            }
+
+            try {
+                tmpOut = mmSocket.outputStream
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when creating output stream", e)
+            }
+
+            mmInStream = tmpIn!!
+            mmOutStream = tmpOut!!
+        }
+
+        override fun run() {
+            mmBuffer = ByteArray(1024)
+            var numBytes: Int // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    // Read from the InputStream.
+                    numBytes = mmInStream.read(mmBuffer)
+                    // Send the obtained bytes to the UI activity.
+                    val readMsg = mHandler!!.obtainMessage(
+                            MessageConstants.MESSAGE_READ, numBytes, -1,
+                            mmBuffer)
+                    readMsg.sendToTarget()
+                } catch (e: IOException) {
+                    Log.d(TAG, "Input stream was disconnected", e)
+                    break
+                }
             }
         }
 
-        fun cancel(){
-            try{
-                mmSocket?.close()
-            }
-            catch(e : IOException)
-            {
-                Log.e("BluetoothConnection", "close() of connected socket failed")
+        // Call this from the main activity to send data to the remote device.
+        fun write(bytes: ByteArray) {
+            try {
+                mmOutStream.write(bytes)
+
+                // Share the sent message with the UI activity.
+                val writtenMsg = mHandler!!.obtainMessage(
+                        MessageConstants.MESSAGE_WRITE, -1, -1, mmBuffer)
+                writtenMsg.sendToTarget()
+            } catch (e: IOException) {
+                Log.e(TAG, "Error occurred when sending data", e)
+
+                // Send a failure message back to the activity.
+                val writeErrorMsg = mHandler!!.obtainMessage(MessageConstants.MESSAGE_TOAST)
+                val bundle = Bundle()
+                bundle.putString("toast",
+                        "Couldn't send data to the other device")
+                writeErrorMsg.data = bundle
+                mHandler.sendMessage(writeErrorMsg)
             }
         }
+
+        // Call this method from the main activity to shut down the connection.
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = "MY_APP_DEBUG_TAG"
+        private val ARDUINO_BT_NAME = "Makebot"
     }
 }
