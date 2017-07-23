d2aae01e74ecf741633e16d5f2632699967394ad
mattdarc <darcangelo.matthew@gmail.com>
2017-07-23 19:57:38 -0400

    removed conflict text in MoveForward

--- app/src/main/kotlin/eldp/robotminediffuser/data/ForwardCommand.kt
+++ app/src/main/kotlin/eldp/robotminediffuser/data/ForwardCommand.kt
@@ -3,8 +3,4 @@ package eldp.robotminediffuser.data
 /**
  * Created by yisuo on 7/17/17.
  */
-<<<<<<< HEAD:app/src/main/kotlin/eldp/robotminediffuser/data/ForwardCommand.kt
 data class ForwardCommand (val forward : Boolean)
-=======
-data class MoveCommand (val distance : Double)
->>>>>>> implement-bluetooth:app/src/main/kotlin/eldp/robotminediffuser/data/MoveCommand.kt
